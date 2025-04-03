2a0a189ccfe3678c5d60ecd8eac3993829a3650a
jettpbaker <jett.p.baker@gmail.com>
2025-04-03 16:31:31 +1000

    setup cors and adjust package.json to allow for railway deployment

--- server.js
+++ server.js
@@ -4,8 +4,27 @@ import posts from './router/posts.js'
 import comments from './router/comments.js'
 import auth from './router/auth.js'
 import { isAuthed, isAdmin } from './middleware/authMiddleware.js'
+import cors from 'cors'
 
 const app = express()
+
+const allowedOrigins = [process.env.CLIENT_URL]
+
+console.log('Allowed CORS Origins:', allowedOrigins)
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true)
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+      return callback(new Error(msg), false)
+    }
+    return callback(null, true)
+  },
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
