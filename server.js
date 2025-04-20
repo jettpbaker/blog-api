bdccf0935c2a7125322bc7f1f3e4c3b070dfc0d0
jettpbaker <jett.p.baker@gmail.com>
2025-04-20 22:16:53 +1000

    started to set up docker. this was torture. wsl and docker is weird. not finished. maybe

--- server.js
+++ server.js
@@ -6,27 +6,37 @@ import { errorHandler } from './middleware/errorHandler.js'
 
 const app = express()
 
-const allowedOrigins = [process.env.CLIENT_URL]
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`)
+  next()
+})
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true)
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
-      return callback(new Error(msg), false)
-    }
-    return callback(null, true)
-  },
-  credentials: true,
-}
+// const allowedOrigins = [process.env.CLIENT_URL]
 
-app.use(cors(corsOptions))
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true)
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+//       return callback(new Error(msg), false)
+//     }
+//     return callback(null, true)
+//   },
+//   credentials: true,
+// }
+
+// app.use(cors(corsOptions))
+
+// app.use(cors({ origin: true, credentials: true }))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/posts', posts)
 app.use('/api/users', users)
+
+// Health check endpoint
+app.get('/', (req, res) => res.send('OK'))
 
 app.use(errorHandler)
 
